a4d57485b55d6610d80af89063741295399f7b55
tehshaw <25413002+tehshaw@users.noreply.github.com>
2021-10-21 21:44:17 -0400

    correct pathing for http requests

--- server.js
+++ server.js
@@ -15,17 +15,17 @@ app.use(express.static('public'));
 
 // GET Route for homepage
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+  res.sendFile('./public/index.html')
 );
 
 // GET Route for notes page
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
+  res.sendFile('./public/notes.html')
 );
 
 // GET Route for feedback page
 app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/404.html'))
+  res.sendFile('./public/404.html')
 );
 
 app.listen(PORT, () =>
