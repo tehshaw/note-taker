bfbd0fbfc2fe06f9a4e792a24cc19825018409e0
tehshaw <25413002+tehshaw@users.noreply.github.com>
2021-10-21 21:58:16 -0400

    correct typo in listening port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const notes = require('./routes/notes');
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
